0d54cad83eb65a1f4f9cf35c24346e1b5c3b99d8
staspv <staspv2008@gmail.com>
2025-01-06 17:17:02 +0300

    условные переменные

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::{collections::VecDeque, sync::Mutex, thread, time::Duration};
+use std::{collections::VecDeque, sync::{Condvar, Mutex}, thread, time::Duration};
 const COUNT: i32 = 20;
 fn main() {
-    thread_park();
+    condvar();
 }
 
+#[allow(dead_code)]
 fn thread_park() {
     let queue = Mutex::new(VecDeque::new());
     thread::scope(|s| {
@@ -22,6 +23,34 @@ fn thread_park() {
         for i in 0..COUNT {
             queue.lock().unwrap().push_back(i);
             t.thread().unpark();
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+}
+
+fn condvar(){
+    let queue = Mutex::new(VecDeque::new());
+    let not_empty = Condvar::new();
+    thread::scope(|s|{
+        s.spawn(||{
+            loop {
+                let mut q = queue.lock().unwrap();
+                let item:i32 = loop {
+                    if let Some(item) = q.pop_front(){
+                        break item;
+                    }
+                    else {
+                        q = not_empty.wait(q).unwrap();
+                    }
+                };
+                drop(q);
+                dbg!(item);
+            }
+        });
+
+        for i in 0..COUNT{
+            queue.lock().unwrap().push_back(i);
+            not_empty.notify_one();
             thread::sleep(Duration::from_millis(100));
         }
     });
