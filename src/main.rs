590148c1405c26de8d433a119b3aa87ec6da73dd
staspv <staspv2008@gmail.com>
2025-01-06 16:47:38 +0300

    парковка потоков

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{collections::VecDeque, sync::Mutex, thread, time::Duration};
+const COUNT:i32 = 20;
 fn main() {
     let queue = Mutex::new(VecDeque::new());
     thread::scope(|s|{
@@ -6,16 +7,19 @@ fn main() {
             let item = queue.lock().unwrap().pop_front();
             if let Some(item) = item {
                 dbg!(item);
+                if item == COUNT - 1{
+                    break;
+                }
             }
             else {
                 thread::park();
             }
         });
 
-        for i in 0.. {
+        for i in 0..COUNT {
             queue.lock().unwrap().push_back(i);
             t.thread().unpark();
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(Duration::from_millis(100));
         }
     });
 }
